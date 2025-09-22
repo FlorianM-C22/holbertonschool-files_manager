f3ba9cd404e515e223fb797cc5faea79464b4b0e
FlorianM-C22 <7484@holbertonstudents.com>
2025-09-22 15:32:28 +0200

    npx eslint . --fix

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,21 +1,21 @@
-import dbClient from '../utils/db';
 import sha1 from 'sha1';
+import dbClient from '../utils/db';
 
 class UsersController {
-    static async postNew(req, res) {
-        const { email, password } = req.body;
-        if (!email) {
-            return res.status(400).json({ error: 'Missing email' });
-        }
-        if (!password) {
-            return res.status(400).json({ error: 'Missing password' });
-        }
-        if (await dbClient.findUserByEmail(email)) {
-            return res.status(400).json({ error: 'Already exist' });
-        }
-        const user = await dbClient.createUser(email, sha1(password));
-        return res.status(201).json({ id: user._id, email });
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
     }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+    if (await dbClient.findUserByEmail(email)) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+    const user = await dbClient.createUser(email, sha1(password));
+    return res.status(201).json({ id: user._id, email });
+  }
 }
 
 export default UsersController;
