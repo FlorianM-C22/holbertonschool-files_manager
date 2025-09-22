d04b134a1aa2c5128ef70fdf9a9656de314e2e06
FlorianM-C22 <7484@holbertonstudents.com>
2025-09-22 14:50:30 +0200

    Fix task + add rules to eslintr

---
+++ Dumb-stuff.md
@@ -0,0 +1,5 @@
+- Les dépendances de l'ère des dinosaures
+- Obligé d'utiliser promisify pour faire fonctionner le client redis. Indiqué nulle part
+- Un p'tit rappel d'où on a utilisé Mongo peut être ? Histoire de setup un truc propre. Obligé de faire tourner sous docker et de faire de la magie noire
+pour tester en local avec docker
+- Un fichier .eslintr mais on a des checks de linting. Bah on met les règles dedans au pire et on s'inflige plus ça ? autoformatonsave, tu connais ?
--- utils/db.mjs
+++ utils/db.mjs
@@ -1,41 +1,41 @@
 import mongo from 'mongodb';
 
 class DBClient {
-    constructor() {
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || 27017;
-        const database = process.env.DB_DATABASE || 'files_manager';
-        const url = `mongodb://${host}:${port}/`;
-        this.client = new mongo.MongoClient(url);
-        this.db = null;
-        
-        this.client.connect((error, client) => {
-            if (error) {
-                console.log('Error connecting to MongoDB:', error);
-            } else {
-                console.log('Connected to MongoDB');
-                this.db = client.db(database);
-            }
-        });
-    }
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/`;
+    this.client = new mongo.MongoClient(url);
+    this.db = null;
 
-    isAlive() {
-        return this.client.isConnected();
-    }
+    this.client.connect((error, client) => {
+      if (error) {
+        console.log('Error connecting to MongoDB:', error);
+      } else {
+        console.log('Connected to MongoDB');
+        this.db = client.db(database);
+      }
+    });
+  }
 
-    async nbUsers() {
-        if (this.db) {
-            return await this.db.collection('users').countDocuments();
-        }
-        return 0;
-    }
+  isAlive() {
+    return this.client.isConnected();
+  }
 
-    async nbFiles() {
-        if (this.db) {
-            return await this.db.collection('files').countDocuments();
-        }
-        return 0;
+  async nbUsers() {
+    if (this.db) {
+      return this.db.collection('users').countDocuments();
     }
+    return 0;
+  }
+
+  async nbFiles() {
+    if (this.db) {
+      return this.db.collection('files').countDocuments();
+    }
+    return 0;
+  }
 }
 
 const dbClient = new DBClient();
